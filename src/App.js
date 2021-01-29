403dfbdde9cebb672735f763d58984bc8644429f
Karthikk <karthikkrishnaraj@syncfusion.com>
2021-01-29 14:20:48 +0530

    Added Shop page with basic functionalities

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import React from 'react';
 import './App.css';
 import {Route} from 'react-router-dom'
 import HomePage from './Pages/HomePage/homepage.component';
-
-const HatsPage = (props) => {
-   console.log(props);
-  return (
-      
-      <h1>HatsPage</h1>
-   )
-}
+import ShopPage from "./Pages/ShopPage/shoppage.component";
 
 class App extends React.Component
 {
  render() { return (
     <div >
      <Route exact path='/' component= {HomePage} />
-     <Route exact path='/hatsPage' component= {HatsPage} />
+     <Route exact path='/shop' component= {ShopPage} />
     </div>
  )
  }
